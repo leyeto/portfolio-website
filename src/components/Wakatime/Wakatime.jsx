8dbed578a9e09670b16a050b2d8268f7fb3fd631
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2023-12-04 11:17:30 +0000

    removed part of wakatime activity

--- src/components/Wakatime/Wakatime.jsx
+++ src/components/Wakatime/Wakatime.jsx
@@ -5,17 +5,17 @@ const Wakatime = () => {
     <div className="wakatime">
       <div className="wakatime__divider">
         <h2 className="wakatime__code-activity">Wakatime Coding Activity</h2>
-        <p className="wakatime__code-heading">Wakatime for the last years</p>
+        {/* <p className="wakatime__code-heading">Wakatime for the last years</p>
         <figure className="wakatime__activity-fig">
           <embed
             className="wakatime__activity-em"
             src="https://wakatime.com/share/@6b62278f-1f06-4dce-90a0-e447c9e59880/0deb259b-5513-41e6-b392-062f4eb2309f.svg"
           ></embed>
-        </figure>
+        </figure> */}
       </div>
       <div className="wakatime__divider">
         <p className="wakatime__code-heading">
-          Wakatime code languages for the last 30 days
+          Wakatime code languages for the last 30 days using wakatime API
         </p>
         <figure className="wakatime__languages">
           <embed
